89790d1c40288c3d2fff219a183236779bd5a277
Tao-Kim <taog397@gmail.com>
2020-07-15 11:42:30 +0900

    Update 회원가입 요청 컨텐츠-타입 확인 추가
    json, application/json 아닐시 상태코드 415 반환

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,47 +13,53 @@ const db = mysql.createConnection({
 });
 
 const register = (req, res) => {
-    const {
-        id,
-        password,
-        name
-    } = req.body;
-    console.log("id :", id, "password :", password, "name :", name);
-    console.log("idtype :", typeof(id));
+    if(req.is(["application/json", "json"])) {
+        const {
+            id,
+            password,
+            name
+        } = req.body;
+        console.log("id :", id, "password :", password, "name :", name);
+        console.log("idtype :", typeof(id));
 
 
-    if (!id || !password || !name) {
-        res.status(400).json({
-            message: '잘못된 요청입니다.'
-        });
-    }
-
-
-    const encryptedPassword = encrypto(password);
-
-
-    db.query('INSERT INTO gb_user (u_id, u_password, u_name) VALUES(?, ?, ?)', [id, encryptedPassword, name], (error, results, fields) => {
-        if (error) {
-
-            if (error.errno == 1062) {
-                res.status(409).json({
-                    message: '이미 존재하는 아이디입니다.'
-                });
-            } else {
-                console.log(error);
-                res.status(500).json({
-                    message: '서버에서 오류가 발생했습니다.'
-                })
-            }
-
-        } else {
-            res.status(201).json({
-                message: '회원가입 성공'
+        if (!id || !password || !name) {
+            res.status(400).json({
+                message: '잘못된 회원정보입니다.'
             });
         }
-    });
 
 
+        const encryptedPassword = encrypto(password);
+
+
+        db.query('INSERT INTO gb_user (u_id, u_password, u_name) VALUES(?, ?, ?)', [id, encryptedPassword, name], (error, results, fields) => {
+            if (error) {
+
+                if (error.errno == 1062) {
+                    res.status(409).json({
+                        message: '이미 존재하는 아이디입니다.'
+                    });
+                } else {
+                    console.log(error);
+                    res.status(500).json({
+                        message: '서버에서 오류가 발생했습니다.'
+                    })
+                }
+
+            } else {
+                res.status(201).json({
+                    message: '회원가입 성공'
+                });
+            }
+        });
+    } else {
+        if (!id || !password || !name) {
+            res.status(415).json({
+                message: '잘못된 요청타입입니다.'
+            });
+        }
+    }
 
 }
 
